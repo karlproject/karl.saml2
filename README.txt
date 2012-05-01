4aaf540c4e6b68427698aa0b21ddf4d56b29cf15
Tres Seaver <tseaver@palladion.com>
2012-05-01 09:59:28 -0400

    Skeleton.

---
+++ README.txt
@@ -0,0 +1,5 @@
+``karl.saml2 README
+===================
+
+This package provices ``SAML2`` integration for the KARL knowledge-management
+platform.
---
+++ setup.py
@@ -0,0 +1,41 @@
+__version__ = '0.1dev'
+
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.txt')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+REQUIRES = ['setuptools', 'karl']
+
+setup(name='karl.saml2',
+      version=__version__,
+      description='SAML2 integration for KARL',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        ],
+      keywords='web wsgi karl',
+      author="Open Society Institute",
+      author_email="osi-dev@lists.palladion.com",
+      url="http://launchpad.net/~teamkarl",
+      license="GPL",
+      packages=find_packages(),
+      include_package_data=True,
+      namespace_packages=['karl'],
+      zip_safe=False,
+      #tests_require = require,
+      install_requires = REQUIRES,
+      test_suite="nose.collector",
+      #entry_points = """\
+      #[console_scripts]
+      #remove_old_tickets = karl.saml2.scripts.karl_saml2_metadta
+      #"""
+)
